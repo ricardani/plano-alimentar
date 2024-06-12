eaf16aca85ffddb945927ae1f19f30d542490a12
Ricardo Soares Silva <ricardani@users.noreply.github.com>
2024-06-12 15:14:19 +0000

    Update PWA config

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,72 @@
+import process from 'node:process'
 import { defineConfig } from 'vite'
-import { VitePWA } from 'vite-plugin-pwa'
+import { ManifestOptions, VitePWA, VitePWAOptions } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react'
+import replace from '@rollup/plugin-replace'
+
+const pwaOptions: Partial<VitePWAOptions> = {
+  registerType: 'autoUpdate',
+  devOptions: {
+    enabled: process.env.SW_DEV === 'true',
+    /* when using generateSW the PWA plugin will switch to classic */
+    type: 'module',
+    navigateFallback: 'index.html',
+  },
+  manifest: {
+    name: 'Plano Alimentar',
+    short_name: 'Plano Alimentar',
+    description: 'Plano alimentar para a semana',
+    theme_color: '#000000',
+    icons: [
+      {
+        "src": "/plano-alimentar/android-chrome-192x192.png",
+        "sizes": "192x192",
+        "type": "image/png"
+      },
+      {
+        "src": "/plano-alimentar/android-chrome-512x512.png",
+        "sizes": "512x512",
+        "type": "image/png"
+      }
+    ],
+    display: 'standalone'
+  }
+}
+
+const replaceOptions = { __DATE__: new Date().toISOString(), preventAssignment: false }
+const claims = process.env.CLAIMS === 'true'
+const reload = process.env.RELOAD_SW === 'true'
+const selfDestroying = process.env.SW_DESTROY === 'true'
+
+if (process.env.SW === 'true') {
+  pwaOptions.srcDir = 'src'
+  pwaOptions.filename = claims ? 'claims-sw.ts' : 'prompt-sw.ts'
+  pwaOptions.strategies = 'injectManifest'
+  ;(pwaOptions.manifest as Partial<ManifestOptions>).name = 'PWA Inject Manifest'
+  ;(pwaOptions.manifest as Partial<ManifestOptions>).short_name = 'PWA Inject'
+  pwaOptions.injectManifest = {
+    minify: false,
+    enableWorkboxModulesLogs: true,
+  }
+}
+
+if (claims)
+  pwaOptions.registerType = 'autoUpdate'
+
+if (reload) {
+  // @ts-expect-error just ignore
+  replaceOptions.__RELOAD_SW__ = 'true'
+}
+
+if (selfDestroying)
+  pwaOptions.selfDestroying = selfDestroying
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/plano-alimentar/',
   plugins: [
     react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      devOptions: {
-        enabled: true
-      },
-      manifest: {
-        name: 'Plano Alimentar',
-        short_name: 'Plano Alimentar',
-        description: 'Plano alimentar para a semana',
-        theme_color: '#000000',
-        icons: [
-          {
-            "src": "/plano-alimentar/android-chrome-192x192.png",
-            "sizes": "192x192",
-            "type": "image/png"
-          },
-          {
-            "src": "/plano-alimentar/android-chrome-512x512.png",
-            "sizes": "512x512",
-            "type": "image/png"
-          }
-        ],
-        display: 'standalone'
-      }
-    })
+    VitePWA(pwaOptions),
+    replace(replaceOptions)
   ],
 })
